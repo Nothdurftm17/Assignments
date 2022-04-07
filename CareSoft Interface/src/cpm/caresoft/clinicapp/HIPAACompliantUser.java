ad3e2a3cb00ec87ca2250b25c6851f2d58107218
Nothdurftm17 <97467294+Nothdurftm17@users.noreply.github.com>
2022-04-07 16:02:23 -0400

    Add files via upload

---
+++ CareSoft Interface/src/cpm/caresoft/clinicapp/HIPAACompliantUser.java
@@ -0,0 +1,6 @@
+package cpm.caresoft.clinicapp;
+
+public interface HIPAACompliantUser {
+	    abstract boolean assignPin(int pin);
+	    abstract boolean accessAuthorized(Integer confirmedAuthID);
+}
